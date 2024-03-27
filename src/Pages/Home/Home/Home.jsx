3f35610ded7c166f35e33ae7145f6bd8445e2478
JannaturNur08 <nur@playense.com>
2024-03-27 15:48:33 +0600

    Popular menu section added in home page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,15 @@
 
 import Banner from '../Banner/Banner';
+import Category from '../Category/Category';
+import PopularMenu from '../PopularMenu/PopularMenu';
 
 const Home = () => {
     return (
         <div>
             <h2>This is Home</h2>
             <Banner></Banner>
+            <Category></Category>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
