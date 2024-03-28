1d43d8f1c7d8d604c43fb0a97d394bedf79ddf8b
JannaturNur08 <nur@playense.com>
2024-03-28 14:48:53 +0600

    firebase project setup with env variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDLPdebjAxy3GiywAGNLsn4Ul0q_Fi5eAs",
-  authDomain: "bistro-boss-client-5ae46.firebaseapp.com",
-  projectId: "bistro-boss-client-5ae46",
-  storageBucket: "bistro-boss-client-5ae46.appspot.com",
-  messagingSenderId: "1069688354134",
-  appId: "1:1069688354134:web:5ba97d4c3ef5e70d3c52f7"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
