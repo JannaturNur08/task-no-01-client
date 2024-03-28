6c437dba7c5935df6275708f444a42d31ab76c75
JannaturNur08 <nur@playense.com>
2024-03-28 23:34:41 +0600

    add to cart data and send to server using axios

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
+import { FaShoppingCart } from "react-icons/fa";
+import useCart from "../../../hooks/useCart";
 
 const Navbar = () => {
 	const { user, logOut } = useContext(AuthContext);
+	const [cart] = useCart();
 
 	const handleLogOut = () => {
 		logOut()
@@ -25,9 +28,19 @@ const Navbar = () => {
 			<li>
 				<Link to="/secret">Secret</Link>
 			</li>
+			<li>
+				<Link to="/dashboard/cart">
+					<button className="btn">
+						<FaShoppingCart className="mr-2"></FaShoppingCart>
+						<div className="badge badge-secondary">
+							+{cart.length}
+						</div>
+					</button>
+				</Link>
+			</li>
 			{user ? (
 				<>
-					<span>{user?.displayName}</span>
+					{/* <span>{user?.displayName}</span> */}
 					<button onClick={handleLogOut} className="btn btn-ghost">
 						LogOut
 					</button>
@@ -41,6 +54,7 @@ const Navbar = () => {
 			)}
 		</>
 	);
+
 	return (
 		<>
 			<div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl bg-black text-white">
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,4 @@
-import {
-    createBrowserRouter,
-    
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Menu from "../Pages/Menu/Menu/Menu";
@@ -9,36 +6,44 @@ import Order from "../Pages/Order/Order/Order";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
 import PrivateRoute from "./PrivateRoute";
-
-
+import Dashboard from "../Layout/Dashboard";
+import Cart from "../Pages/Dashboard/Cart/Cart";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
-        {
-            path: 'menu',
-            element: <PrivateRoute>
-              <Menu></Menu>
-            </PrivateRoute>
-        },
-        {
-            path: 'order/:category',
-            element: <Order></Order>
-        },
-        {
-            path: 'login',
-            element: <Login></Login>
-        },
-        {
-            path: 'signup',
-            element: <SignUp></SignUp>
-        },
-      ]
-    },
-  ]);
+	{
+		path: "/",
+		element: <Main></Main>,
+		children: [
+			{
+				path: "/",
+				element: <Home></Home>,
+			},
+			{
+				path: "menu",
+				element: <Menu></Menu>,
+			},
+			{
+				path: "order/:category",
+				element: <Order></Order>,
+			},
+			{
+				path: "login",
+				element: <Login></Login>,
+			},
+			{
+				path: "signup",
+				element: <SignUp></SignUp>,
+			},
+		],
+	},
+	{
+		path: "dashboard",
+		element: <Dashboard></Dashboard>,
+		children: [
+			{
+				path: "cart",
+				element: <Cart></Cart>,
+			},
+		],
+	},
+]);
