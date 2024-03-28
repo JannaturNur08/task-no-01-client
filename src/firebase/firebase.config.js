b15b17bad0a729fc37878889b0cdb63019b20581
JannaturNur08 <nur@playense.com>
2024-03-28 14:38:51 +0600

    simple login form with validate captcha

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLPdebjAxy3GiywAGNLsn4Ul0q_Fi5eAs",
+  authDomain: "bistro-boss-client-5ae46.firebaseapp.com",
+  projectId: "bistro-boss-client-5ae46",
+  storageBucket: "bistro-boss-client-5ae46.appspot.com",
+  messagingSenderId: "1069688354134",
+  appId: "1:1069688354134:web:5ba97d4c3ef5e70d3c52f7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
