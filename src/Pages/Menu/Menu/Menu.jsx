ba2f35a1200417b95bae58505b592eb4b7528ce4
JannaturNur08 <nur@playense.com>
2024-03-28 11:58:57 +0600

    cover with blur parallax using react parallax

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,15 +1,23 @@
 
 import { Helmet } from 'react-helmet-async';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import Cover from '../../Shared/Cover/Cover';
+import useMenu from '../../../hooks/useMenu';
+import menuImg from '../../../assets/menu/menu-bg.png';
 
 const Menu = () => {
-    
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            
+            <Cover img={menuImg} title="our menu"></Cover>
             {/* main cover */}
            <SectionTitle  subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
            
