5c8f2a2035094a1910dffa7303af5269087f9920
JannaturNur08 <nur@playense.com>
2024-03-29 21:30:24 +0600

    request interceptor and response interceptor added in axiosSecure

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -51,21 +51,19 @@ const AuthProvider = ({ children }) => {
 	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
 			setUser(currentUser);
-            if (currentUser) {
-                // get token and store client
-                const userInfo = { email: currentUser.email };
-                axiosPublic.post('/jwt', userInfo)
-                    .then(res => {
-                        if (res.data.token) {
-                            localStorage.setItem('access-token', res.data.token);
-                        }
-                    })
-            }
-            else {
-                // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
-                localStorage.removeItem('access-token');
-            }
-            setLoading(false);
+			if (currentUser) {
+				// get token and store client
+				const userInfo = { email: currentUser.email };
+				axiosPublic.post("/jwt", userInfo).then((res) => {
+					if (res.data.token) {
+						localStorage.setItem("access-token", res.data.token);
+					}
+				});
+			} else {
+				// TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+				localStorage.removeItem("access-token");
+			}
+			setLoading(false);
 		});
 		return () => {
 			return unsubscribe();
