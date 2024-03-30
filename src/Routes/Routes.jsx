fbed80674942a58600c5567b3499106f7e76987b
JannaturNur08 <nur@playense.com>
2024-03-30 13:54:07 +0600

    UserHome page created for dashboard

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -15,90 +15,86 @@ import ManageItems from "../Pages/Dashboard/ManageItems/ManageItems";
 import UpdateItem from "../Pages/Dashboard/UpdateItem/UpdateItem";
 import Payment from "../Pages/Dashboard/Payment/Payment";
 import PaymentHistory from "../Pages/Dashboard/PaymentHistory/PaymentHistory";
+import AdminHome from "../Pages/Dashboard/AdminHome/AdminHome";
+import Secret from "../Pages/Shared/Secret/Secret";
+import UserHome from "../Pages/Dashboard/UserHome/UserHome";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Main></Main>,
 		children: [
-			{
-				path: "/",
-				element: <Home></Home>,
-			},
-			{
-				path: "menu",
-				element: <Menu></Menu>,
-			},
-			{
-				path: "order/:category",
-				element: <Order></Order>,
-			},
-			{
-				path: "login",
-				element: <Login></Login>,
-			},
-			{
-				path: "signup",
-				element: <SignUp></SignUp>,
-			},
-		],
-	},
-	{
-		path: "dashboard",
-		element: (
-			<PrivateRoute>
-				<Dashboard></Dashboard>
-			</PrivateRoute>
-		),
+		  {
+			  path: '/',
+			  element: <Home></Home>
+		  }, 
+		  {
+			path: 'menu', 
+			element: <Menu></Menu>
+		  },
+		  {
+			path: 'order/:category',
+			element: <Order></Order>
+		  },
+		  {
+			path: 'login',
+			element: <Login></Login>
+		  },
+		  {
+			path: 'signup',
+			element: <SignUp></SignUp>
+		  },
+		  {
+			path: 'secret',
+			element: <PrivateRoute><Secret></Secret></PrivateRoute>
+		  }
+		]
+	  },
+	  {
+		path: 'dashboard',
+		element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
 		children: [
-			{
-				path: "cart",
-				element: <Cart></Cart>,
-			},
-			{
-				path: "payment",
-				element: <Payment></Payment>,
-			},
-			{
-				path: "paymentHistory",
-				element: <PaymentHistory></PaymentHistory>,
-			},
-			// admin only routes
-			{
-				path: "addItems",
-				element: (
-					<AdminRoute>
-						<AddItems></AddItems>
-					</AdminRoute>
-				),
-			},
-			{
-				path: "manageItems",
-				element: (
-					<AdminRoute>
-						<ManageItems></ManageItems>
-					</AdminRoute>
-				),
-			},
-			{
-				path: "updateItem/:id",
-				element: (
-					<AdminRoute>
-						<UpdateItem></UpdateItem>
-					</AdminRoute>
-				),
-				loader: ({ params }) =>
-					fetch(`http://localhost:5000/menu/${params.id}`),
-			},
-
-			{
-				path: "users",
-				element: (
-					<AdminRoute>
-						<AllUsers></AllUsers>
-					</AdminRoute>
-				),
-			},
-		],
-	},
+		  // normal user routes
+		  {
+			path: 'userHome',
+			element: <UserHome></UserHome>
+		  },
+		  {
+			path: 'cart',
+			element: <Cart></Cart>
+		  },
+		  {
+			path: 'payment',
+			element: <Payment></Payment>
+		  },
+		  {
+			path: 'paymentHistory',
+			element: <PaymentHistory></PaymentHistory>
+		  },
+  
+		  // admin only routes
+		  {
+			path: 'adminHome',
+			element: <AdminRoute><AdminHome></AdminHome></AdminRoute>
+		  },
+		  {
+			path: 'addItems',
+			element: <AdminRoute><AddItems></AddItems></AdminRoute>
+		  },
+		  {
+			path: 'manageItems',
+			element: <AdminRoute><ManageItems></ManageItems></AdminRoute>
+		  },
+		  {
+			path: 'updateItem/:id',
+			element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
+			loader: ({params}) => fetch(`https://bistro-boss-server-seven-sage.vercel.app/menu/${params.id}`)
+		  },
+		  {
+			path: 'users',
+			element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
+		  }
+  
+		]
+	  }
 ]);
